17e208b4f3f395116468971f1703739e4ca80f18
Amerecs <altybyk748@gmail.com>
2024-10-19 13:20:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildInvites
+    ]
+});
+
+client.once('ready', () => {
+    console.log("ready");
+});
+
+client.on("messageCreate", async message => {
+    if (message.content === "setup") {
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel("Verify")
+                    .setStyle(ButtonStyle.Success)
+                    .setCustomId("1")
+            );
+
+        await message.delete();
+        await message.channel.send({ components: [row] });
+    }
+});
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isButton()) return;
+
+    if (interaction.customId === "1") {
+        const member = interaction.member;
+        const role = interaction.guild.roles.cache.find(role => role.id === ""); //roleId
+
+        if (role) {
+            await member.roles.add(role);
+            await interaction.reply({ content: "تم تحقق بنجاح", ephemeral: true });
+        } else {
+            await interaction.reply({ content: "Role not found", ephemeral: true });
+        }
+    }
+});
+
+client.login("") //token bot
